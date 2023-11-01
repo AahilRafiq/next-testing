544fb41cbc33796cd09660d0cd506803fb53106c
aahilrafiq <aahil.rafiq136@gmail.com>
2023-11-01 12:44:57 +0530

    ready for final commit

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+export default function About() {
+    
+      return (
+     <>
+     <div>
+        <h1>About</h1>
+        <p>My cool about page</p>
+     </div>
+     <button onClick={()=>alert("Hello")}>ALert</button>
+     </>
+      );
+}
